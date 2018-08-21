1b4aeb3d588e208598467df59ec29c8e62195544
Ali Sufyan <alisufyan327@gmail.com>
2018-08-21 11:21:59 -0700

    react App created

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log(`Started server on port ${port}`);
 });
